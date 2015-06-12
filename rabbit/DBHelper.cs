01a93933aa4718778f1fd75318539b9eb449ab56
zmovahedi <movahedizahra29@gmail.com>
2015-06-12 06:23:37 -0700

    Test

--- rabbit/DBHelper.cs
+++ rabbit/DBHelper.cs
@@ -8,10 +8,10 @@ namespace rabbit
 {
     public static class DbHelper
     {
-        //public static bool Connect(string connectionSting)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        public static bool Connect(string connectionSting)
+        {
+            throw new NotImplementedException();
+        }
 
         public static List<string> GetDatabases(string connectionString)
         {
